e3bb28f351938514793e78c28210047de6639cf9
Kwstas <tsouk88@gmail.com>
2026-05-12 20:03:41 +0300

    Add pytest test suite for load_excel_data

---
+++ test_loader.py
@@ -0,0 +1,19 @@
+import pytest
+from loader import load_excel_data
+
+@pytest.fixture
+def file():
+    file_path = "C:/Users/tsouk/Desktop/python-practice/PhD-Rag/data/stats.xlsx"
+    sheet = "Normal Οικον Βάση"
+    return load_excel_data(file_path , sheet)
+                    
+def test_load(file):
+    assert len(file) == 234
+
+def test_meta(file):
+    check= file[0].metadata
+    assert "region" in check and "indicator" in check and "era" in check and "source" in check
+
+def test_wrongpath():
+    with pytest.raises( FileNotFoundError):
+        load_excel_data("C:/Users/tsouk/Desktop/python-practice/PhD-Rag/stats.xlsx" , "Normal Οικον Βάση" )
