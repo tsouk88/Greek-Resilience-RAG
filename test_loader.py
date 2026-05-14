41b7d2b7e334d719958054a4fc4e08ce2abc6dee
Kwstas <tsouk88@gmail.com>
2026-05-14 12:39:27 +0300

    Skip data-dependent tests in CI environment

--- test_loader.py
+++ test_loader.py
@@ -2,6 +2,8 @@ import pytest
 from loader import load_excel_data
 import os
 
+CI = os.getenv("CI", False)
+
 file_path = os.getenv("FILE_PATH", "data/stats.xlsx")
 
 @pytest.fixture
@@ -9,6 +11,7 @@ def file():
     sheet = "Normal Οικον Βάση"
     return load_excel_data(file_path , sheet)
 
+@pytest.mark.skipif(CI, reason="No Excel file in CI")
 @pytest.mark.parametrize( "sheet" , ["Normal Οικον Βάση",
     "Normal Οικον Βάση - Crisis", 
     "Normal Οικον Βάση - COVID",
@@ -19,14 +22,16 @@ def test_all(sheet):
     docs = load_excel_data(file_path , sheet)
     assert len(docs)>0 
 
-                    
+@pytest.mark.skipif(CI, reason="No Excel file in CI")                   
 def test_load(file):
     assert len(file) == 234
 
+@pytest.mark.skipif(CI, reason="No Excel file in CI")
 def test_meta(file):
     check= file[0].metadata
     assert "region" in check and "indicator" in check and "era" in check and "source" in check
-
+    
+@pytest.mark.skipif(CI, reason="No Excel file in CI")
 def test_wrongpath():
     with pytest.raises( FileNotFoundError):
-        load_excel_data(file_path , "Normal Οικον Βάση" )
+        load_excel_data("fake/path/that/doesnt/exist.xlsx" , "Normal Οικον Βάση" )
